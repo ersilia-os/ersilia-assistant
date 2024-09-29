296a37bc1c0081f3b9e530f1e114cdaf6bc9082b
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-09-30 01:58:41 +0530

    WIP

--- experiments/run_model.py
+++ experiments/run_model.py
@@ -1,38 +1,55 @@
 """
 Runs the input model and stores prompt results in a file.
 """
+import os
+import time
+import sys
+import csv
 from openai import OpenAI
 
+model = sys.argv[1]
+DIR = os.path.dirname(os.path.abspath(__file__))
 
 client = OpenAI(
-    base_url="http://localhost:8080/v1", #
+    base_url="http://localhost:8080/v1", # Whichever port you are using
     api_key = "sk-no-key-required"
 )
-# completion = client.chat.completions.create(
-#     model="LLaMA_CPP",
-#     messages=[
-#         {"role": "system", "content": "You are ChatGPT, an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests."},
-#         {"role": "user", "content": "Write a limerick about python exceptions"}
-#     ]
-# )
-# print(completion.choices[0].message)
 
 def read_prompts(file_path):
     with open(file_path, "r") as file:
-        return file.readlines()
+        lines = file.readlines()
+        lines = list(filter(lambda x: x != "\n", lines))
+        return lines
     
-def query_model(model, prompt):
+def query_model(prompt):
     completion = client.chat.completions.create(
-        # model="LLaMA_CPP",
+        model="LLaMA_CPP",
+        messages=[
+            {"role": "system", "content": "You are Llama an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests."},
+            {"role": "user", "content": prompt}
+        ]
     )
-    # TODO: Implement the model query
+    return completion.choices[0].message.content
 
-def store_query_results(results):
-    # Store the results in a file
-    pass
+def store_query_results(model, results):
+    header = ["prompt", "response", "total_time"]
+    
+    with open(os.path.join(DIR, f"{model}_results.csv"), "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        for result in results:
+            writer.writerow(result)
 
 if __name__ == "__main__":
-    prompts = read_prompts("prompts.txt")
+    prompts = read_prompts(os.path.join(DIR, "prompts.txt"))
+    results = []
+
     for prompt in prompts:
-        query_model("LLaMA_CPP", prompt)
-        # Store the results in a file
+        res = [prompt]
+        start = time.time()
+        res.append(query_model(prompt))
+        end = time.time()
+        tot_time = end - start
+        res.append(tot_time)
+        results.append(res)
+    store_query_results(model, results)
