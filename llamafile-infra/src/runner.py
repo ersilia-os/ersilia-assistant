96a61b13320bee2b14e6f0755d31f7497c66e386
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-10-29 19:01:43 +0530

    Add deployment steps for running llamafile on lambda

---
+++ llamafile-infra/src/runner.py
@@ -0,0 +1,27 @@
+import asyncio
+
+async def start_llamafile():
+    process = await asyncio.create_subprocess_shell(
+    "/opt/llamafile/command.sh",
+    stdout=asyncio.subprocess.PIPE,
+    stderr=asyncio.subprocess.PIPE
+    )
+
+    async def read_output(stream):
+        while True:
+            line = await stream.readline()
+            if not line:
+                break
+            output = line.decode()
+            print(output, end='')
+            if "llama server listening" in output:
+                return
+
+    await asyncio.gather(
+    read_output(process.stdout),
+    read_output(process.stderr)
+    )
+
+    await process.wait()
+
+asyncio.run(start_llamafile())
