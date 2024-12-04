bfd1895c38aba8db61e4efe7083eb62b222e9d80
GemmaTuron <gemma@ersilia.io>
2024-12-04 06:02:51 +0300

    responses edited

--- demo/app.py
+++ demo/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-import json
 import os
 from inputs import disease, dataset, objective, prompts, about
 from PIL import Image
@@ -8,7 +7,7 @@ from PIL import Image
 root = os.path.dirname(__file__)
 
 # Function for the typing effect
-def type_text(text, placeholder, delay=0.02):
+def type_text(text, placeholder, delay=0.0):
     typed_text = ""
     for char in text:
         typed_text += char
@@ -17,10 +16,9 @@ def type_text(text, placeholder, delay=0.02):
             border: 1px solid #e6e6e6; 
             padding: 10px; 
             border-radius: 5px; 
-            background-color: #f9f9f9; 
+            background-color: #d2d2d0; 
             color: #333; 
-            font-family: monospace; 
-            white-space: pre-wrap; 
+            font-family: sans-serif;  
             word-wrap: break-word;">
             {typed_text}
         </div>
@@ -52,12 +50,12 @@ images = load_images()
 # Main application
 st.set_page_config(layout="wide", page_title="Ersilia Self Service")
 st.sidebar.image(os.path.join(root, "images", "Ersilia_Brand_white_transp.png"))
-st.sidebar.markdown("The [Ersilia Open Source Initiative](www.ersilia.io) is a tech **nonprofit organization** aimed at supporting research **scientists in the Global South** with **AI/ML tools for drug discovery and infectious disease research**.")
+st.sidebar.markdown("The [Ersilia Open Source Initiative](www.ersilia.io) is a tech **nonprofit organization** aimed at supporting **scientists in the Global South** with **AI/ML tools for drug discovery and infectious disease research**.")
 for i in range(3):
     st.sidebar.write(about[i])
 
 st.title("Ersilia Assistant")
-st.write("Welcome to the Ersilia Assistant Demo, where you can put yourself in the shoes of a biomedical scientist! Select your preferred research program from the options below and see how AI can help you!")
+st.write("Welcome to the Ersilia Assistant Demo, where you can put yourself in the shoes of a biomedical scientist! Select your preferred research program from the options below and see how can AI help you!")
 
 # Initialize session state for selections
 if "selections" not in st.session_state:
@@ -71,35 +69,40 @@ with col1:
     st.image(images["M"])
     for key, value in disease.items():
         if key == "M" and st.button(value, key=key, use_container_width=True):
+            st.toast("Malaria selected")
             st.session_state.selections["disease"] = key
 
 with col2:
     st.image(images["T"])
     for key, value in disease.items():
         if key == "T" and st.button(value, key=key, use_container_width=True):
+            st.toast("Tuberculosis selected")
             st.session_state.selections["disease"] = key
 
 with col3:
     st.image(images["C"])
     for key, value in disease.items():
         if key == "C" and st.button(value, key=key, use_container_width=True):
+            st.toast("COVID-19 selected")
             st.session_state.selections["disease"] = key
     
 
 # Step 2: Starting Library
-st.subheader("Starting library:")
+st.subheader("Compound collection:")
 col1, col2 = st.columns(2)
 
 with col1:
     st.image(images["N"])
     for key, value in dataset.items():
         if key == "N" and st.button(value, key=key, use_container_width=True):
+            st.toast("Natural products collection")
             st.session_state.selections["dataset"] = key
 
 with col2:
     st.image(images["S"])
     for key, value in dataset.items():
         if key == "S" and st.button(value, key=key, use_container_width=True):
+            st.toast("Synthetic compounds collection")
             st.session_state.selections["dataset"] = key
 
 # Step 3: Objective
@@ -111,18 +114,21 @@ with col1:
     st.image(images["Select"])
     for key, value in objective.items():
         if key == "Select" and st.button(value, key=key, use_container_width=True):
+            st.toast("Best candidates query")
             st.session_state.selections["objective"] = key
 
 with col2:
     st.image(images["Toxic"])
     for key, value in objective.items():
         if key == "Toxic" and st.button(value, key=key, use_container_width=True):
+            st.toast("Toxicity query")
             st.session_state.selections["objective"] = key
 
 with col3:
     st.image(images["Expand"])
     for key, value in objective.items():
         if key == "Expand" and st.button(value, key=key, use_container_width=True):
+            st.toast("Library expansion query")
             st.session_state.selections["objective"] = key
 
 # Display currently selected options
@@ -133,7 +139,7 @@ st.write(f"**Objective:** {objective.get(st.session_state.selections['objective'
 
 # Generate Query button
 if None not in st.session_state.selections.values():
-    if st.button("Generate Query"):
+    if st.button(":thinking_face: Generate Query"):
         query_key = (
             st.session_state.selections["disease"],
             st.session_state.selections["dataset"],
@@ -144,6 +150,10 @@ if None not in st.session_state.selections.values():
         st.session_state['query_generated'] = True  # Mark query as generated
         placeholder = st.empty()  # Placeholder for typing effect
         type_text(query, placeholder)
+    else:
+        if "query_generated" in st.session_state:
+            placeholder = st.empty()
+            type_text(st.session_state.query, placeholder, delay=0)
 
 else:
     st.info("Please complete all selections to generate a query.")
@@ -151,10 +161,10 @@ else:
 st.write("")
 # Ask the Ersilia Assistant Button appears only after query is generated
 if st.session_state.get('query_generated', False):
-    if st.button("Ask the Ersilia Assistant"):
+    if st.button(":robot_face: Ask the Ersilia Assistant"):
         # Load the response text from the JSON file
         file_name = "_".join(st.session_state.selections.values()) + ".txt"
-        with open(os.path.join(root, "responses", file_name), "r") as f:
+        with open(os.path.join(root, "responses_edited", file_name), "r") as f:
             response_text = f.read()
 
         # Display the response with the typing effect
@@ -162,4 +172,4 @@ if st.session_state.get('query_generated', False):
         type_text(response_text, response_placeholder)
 
 # Reset app
-        st.button('Reset', on_click=clear_cache)
+        st.button(':recycle: Reset', on_click=clear_cache)
